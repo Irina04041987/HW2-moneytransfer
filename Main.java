73c1a51fa42d36197bfa0d0aa663fa7790af05c6
Irina Platonova <crazyangeli@yandex.ru>
2021-01-16 18:26:41 +0300

    add money transfer

--- Main.java
+++ Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
+        System.out.println ("Итоговое значение "+total);
+    }
 }
